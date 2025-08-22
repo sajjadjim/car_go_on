b81c34254e723c61ff410344fede384dc5f93538
SAJJAD HOSAIN JIM <sajjadjim15@gmail.com>
2025-08-22 17:16:18 +0600

    genarated a redme file here

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,27 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     images: {
+//         domains: ['www.themealdb.com'],
+//     },
+//     async redirects() {
+//         return [
+//             {
+//                 source: '/products/add',
+//                 destination: '/dashboard/products/add',
+//                 permanent: true,
+//             }
+//         ];
+//     }
+// };
+
+// export default nextConfig;
+
+
+// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['www.themealdb.com'],
-    },
-    async redirects() {
-        return [
-            {
-                source: '/products/add',
-                destination: '/dashboard/products/add',
-                permanent: true,
-            }
-        ];
-    }
+  // your config here
+  // e.g. images: { remotePatterns: [] }
 };
 
-export default nextConfig;
+export default nextConfig;   // ✅ ESM export
